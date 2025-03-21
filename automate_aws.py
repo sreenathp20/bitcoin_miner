128b535a974bd87ab7eb3567a12bec3b19d26950
sreenath puthiyaveettil <sreenath@sreenaths-MacBook-Pro.local>
2025-03-22 00:19:44 +0530

    automate

--- automate_aws.py
+++ automate_aws.py
@@ -3,48 +3,79 @@ import jmespath
 import paramiko
 import time
 from db import MongoDb
+import os
 
-client = boto3.client('ec2')
-response = client.describe_instances()
-
-myData = jmespath.search("Reservations[].Instances[].[NetworkInterfaces[0].OwnerId, InstanceId, InstanceType, State.Name, Placement.AvailabilityZone, PrivateIpAddress, PublicIpAddress, KeyName, [Tags[?Key=='Name'].Value] [0][0]]", response)
-
-pass
 
 m = MongoDb('localhost')
 extranonce = m.read('extranonce', {})
 if len(extranonce) > 0:
-    extranonce = extranonce['extranonce']
+    extranonce = extranonce[0]['extranonce']
 else:
     extranonce = 0
 
-for md in myData:
-    if md[8] != 'b_core':
-        host = md[6]
-        keyfilename = '/Users/sreenath/Downloads/b000001.pem'
-        k = paramiko.RSAKey.from_private_key_file(keyfilename)
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        user = 'ubuntu'
-        ssh.connect(hostname=host, username=user, pkey=k)
-        sleeptime = 0.001
-        ssh.exec_command('sudo su \n mkdir btc ') 
-        # chan = ssh_transp.open_session()
-        ssh.exec_command('sudo su \n cd btc \n git clone https://github.com/sreenathp20/bitcoin_miner.git') 
-        # chan = ssh_transp.open_session()
-        ssh.exec_command('sudo su \n apt update \n yes | sudo apt install python3.12-venv \n cd btc \n mkdir logs \n python3 -m venv venv \n source venv/bin/activate') 
+m = MongoDb('localhost')
+processed = m.read('processed', {})
 
-        ssh.exec_command('sudo su \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+extranonce+' > /home/ubuntu/btc/logs/1.log &') 
+if len(processed) > 0:
+    processed = processed[0]['processed']
+else:
+    processed = []
 
-        ssh.exec_command('sudo su \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+extranonce+' > /home/ubuntu/btc/logs/2.log &') 
+path = '/Users/sreenath/Documents/bitcoin_code2/aws_credentials'
+lines = os.popen("ls -l "+path).readlines()
+for line in lines[1:]:
+    file = line.split(" ")[-1][:-1]
+    #os.popen("cp "+path+"/"+file+" /Users/sreenath/.aws/credentials")
+    pass
+    if file in processed:
+        continue
+    processed.append(file)
+    session = boto3.Session(profile_name=file)
+    client = session.client('ec2')
+    #client = boto3.client('ec2')
+    response = client.describe_instances()
 
-        extranonce += 1000
+    myData = jmespath.search("Reservations[].Instances[].[NetworkInterfaces[0].OwnerId, InstanceId, InstanceType, State.Name, Placement.AvailabilityZone, PrivateIpAddress, PublicIpAddress, KeyName, [Tags[?Key=='Name'].Value] [0][0]]", response)
 
 
-ssh.close()
+    for md in myData:
+        if md[8] != 'b_core':
+            host = md[6]
+            print("host: ", host)
+            keyfilename = '/Users/sreenath/Downloads/'+file+'.pem'
+            k = paramiko.RSAKey.from_private_key_file(keyfilename)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            user = 'ubuntu'
+            ssh.connect(hostname=host, username=user, pkey=k)
+            sleeptime = 0.001
+            ssh.exec_command('sudo su \n mkdir btc ') 
+            # chan = ssh_transp.open_session()
+            ssh.exec_command('sudo su \n cd btc \n git clone https://github.com/sreenathp20/bitcoin_miner.git') 
+            # chan = ssh_transp.open_session()
+            ssh.exec_command('sudo su \n sudo apt update \n yes | sudo apt install python3.12-venv \n cd btc \n mkdir logs \n python3 -m venv venv \n source venv/bin/activate \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+str(extranonce)+' > /home/ubuntu/btc/logs/1.log & \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+str(extranonce+1000)+' > /home/ubuntu/btc/logs/2.log &') 
+
+            # ssh.exec_command('sudo su \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+str(extranonce)+' > /home/ubuntu/btc/logs/1.log &') 
+            # extranonce += 1000
+            # ssh.exec_command('sudo su \n /home/ubuntu/btc/venv/bin/python /home/ubuntu/btc/bitcoin_miner/ntgbtminer.py sreenath 1DpMnorqAqtuEuZhD3tNujL83QhD9qEvG4 0 '+str(extranonce)+' > /home/ubuntu/btc/logs/2.log &') 
+
+            extranonce += 2000
+    m = MongoDb('localhost')
+    m.delete("processed", {})
+
+    m = MongoDb('localhost')
+    data = {
+        "processed": processed
+    }
+    m.insertMany('processed', [data])
+
+    ssh.close()
+
+m = MongoDb('localhost')
+m.delete("extranonce", {})
 
 m = MongoDb('localhost')
 data = {
     "extranonce": extranonce
 }
-extranonce = m.insertMany('extranonce', {})
+extranonce = m.insertMany('extranonce', [data])
---
+++ metrics_aws.py
@@ -0,0 +1,50 @@
+import boto3
+import jmespath
+import paramiko
+import time
+from db import MongoDb
+import os
+
+
+tot_prc = 0
+path = '/Users/sreenath/Documents/bitcoin_code2/aws_credentials'
+lines = os.popen("ls -l "+path).readlines()
+for line in lines[1:]:
+    file = line.split(" ")[-1][:-1]
+    #os.popen("cp "+path+"/"+file+" /Users/sreenath/.aws/credentials")
+    print("account: ", file)
+    client = None
+    session = boto3.Session(profile_name=file)
+    client = session.client('ec2')
+    #client = boto3.client('ec2')
+    response = client.describe_instances()
+    myData = None
+    myData = jmespath.search("Reservations[].Instances[].[NetworkInterfaces[0].OwnerId, InstanceId, InstanceType, State.Name, Placement.AvailabilityZone, PrivateIpAddress, PublicIpAddress, KeyName, [Tags[?Key=='Name'].Value] [0][0]]", response)
+
+    prc = 0
+    for md in myData:
+        if md[8] != 'b_core':
+            host = md[6]
+            print("host: ", host)
+            keyfilename = '/Users/sreenath/Downloads/'+file+'.pem'
+            k = paramiko.RSAKey.from_private_key_file(keyfilename)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            user = 'ubuntu'
+            ssh.connect(hostname=host, username=user, pkey=k)
+            sleeptime = 0.001
+            inp,out,err = ssh.exec_command('ps aux | grep ntgbtminer', get_pty=True) 
+            #print("out: ", out)
+            l = out.readlines()
+            
+            if len(l) == 4:
+                prc += 2                
+            else:
+                np = len(l) - 2
+                prc += np
+                print(f"account: {file} host: {host} {np} process(es) running")
+            ssh.close()
+    tot_prc += prc
+    print(f"{len(myData)} hosts, account: {file}")
+
+print(f"{tot_prc} process(es) running")
